6e2aeee721eddcfb290d57c3c1339fe7c7798631
beargame123 <hoyoung7827@gmail.com>
2022-05-17 23:41:33 +0900

    ➕WebSecuirtyConfig,  DetailService, UserRepository추가

--- jwtTest/src/main/java/com/example/jwttest/config/WebSecurityConfig.java
+++ jwtTest/src/main/java/com/example/jwttest/config/WebSecurityConfig.java
@@ -1,23 +1,61 @@
 package com.example.jwttest.config;
 
+import com.example.jwttest.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.http.HttpServlet;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/localost:/1010/**");
+    }
+
+    //암호화에 필요한 PasswordEncoder를 Bean으로 등록합니다.
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // authenticatonManager를 Bean 등록합니다.
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
 
     @Override
     public void configure(HttpSecurity http)throws Exception{
         http
-                .authorizeRequests()
+                .httpBasic().disable() // rest api 만을 고려하여 기본 설정을 해제한다.
+                .csrf().disable() // csrf 보안 토큰 disable처리.
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //토큰 기반이기때문에 세션 사용 안함.
+             .and()
+                .authorizeRequests() // 요청에 대한 사용권한 체크
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasRole("USER")
-                .antMatchers("/**").permitAll();
+                .anyRequest().permitAll() //나머지 요청은 모두 다 사용가능
+             .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+        // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣음.
 
     }
 }
